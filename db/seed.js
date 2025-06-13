f6b2b24efffcc41a1b6587749ace8a1deb93abf5
Taibyonas@gmail.com <Taibyonas@gmail.com>
2025-06-12 20:18:47 -0500

    yt

--- db/seed.js
+++ db/seed.js
@@ -16,3 +16,26 @@ async function seedUsers(){
 async function seedOrders(){
 
 }
+
+
+import db from "#db/client.js";
+import { createUser } from "./queries/users.js";
+
+await db.connect();
+
+await seedUsers();
+
+await db.end();
+console.log("🌱 Database seeded.");
+
+async function seedUsers() {
+  console.log("Seeding users...");
+
+  await db.query(`DELETE FROM users;`);
+
+  await createUser('yoyo', 'pass123');
+  await createUser('yoni', 'password456');
+  await createUser('alem', 'secure789');
+
+  console.log("✅ Users seeded.");
+}
