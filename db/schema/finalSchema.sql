d9b96161b3e48304655ffe89c78f0c5f7534cd5f
Lyla Lynn <lyladlynn@gmail.com>
2025-08-01 16:59:40 -0500

    fixing seed data not working

--- db/schema/finalSchema.sql
+++ db/schema/finalSchema.sql
@@ -1,8 +1,8 @@
-\i dropTables.sql
+\i db/schema/dropTables.sql
 
-\i users.sql
-\i products.sql
-\i orders.sql
-\i carts.sql
-\i reviews.sql
-\i orderItems.sql
+\i db/schema/users.sql
+\i db/schema/products.sql
+\i db/schema/orders.sql
+\i db/schema/carts.sql
+\i db/schema/reviews.sql
+\i db/schema/orderItems.sql
--- db/schema/users.sql
+++ db/schema/users.sql
@@ -1,5 +1,8 @@
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
+  first_name TEXT NOT NULL,
+  last_name TEXT NOT NULL,
+  email TEXT NOT NULL UNIQUE,
   username TEXT NOT NULL UNIQUE,
   password TEXT NOT NULL
 );
